9a357e2ef17bb15e5ab94f41ec772e5e7d96f945
thorinf <thorin.j.a.farnsworth@gmail.com>
2025-10-12 01:28:40 +0100

    feat(firmware): basic gate output with midi working
    Signed-off-by: thorinf <thorin.j.a.farnsworth@gmail.com>

--- firmware/src/hardware_config.h
+++ firmware/src/hardware_config.h
@@ -3,3 +3,12 @@
 #define F_CPU 16000000UL
 #define BAUD 31250UL
 #define MY_UBRR ((F_CPU / (16UL * BAUD)) - 1)
+
+// Gate configuration
+#define NUM_GATES 8
+#define GATE_PORT_B PORTB
+#define GATE_PORT_D PORTD
+#define GATE_DDR_B DDRB
+#define GATE_DDR_D DDRD
+#define GATE_PIN_0 PB0
+#define GATE_PIN_1 PD1
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,6 +3,7 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <stdint.h>
+#include <util/delay.h>
 
 #define RB_SIZE 64
 #define RB_MASK (RB_SIZE - 1)
@@ -41,7 +42,50 @@ static inline uint8_t rb_pop(uint8_t *out) {
   return 1;
 }
 
+static inline void gate_set(uint8_t gate_index, uint8_t state) {
+  volatile uint8_t *port_reg;
+  uint8_t mask;
+
+  if (gate_index == 0) {
+    port_reg = &GATE_PORT_B;
+    mask = (uint8_t)(1 << GATE_PIN_0);
+  } else {
+    port_reg = &GATE_PORT_D;
+    mask = (uint8_t)(1 << (GATE_PIN_1 + gate_index - 1));
+  }
+
+  if (state) {
+    *port_reg |= mask;
+  } else {
+    *port_reg &= (uint8_t)~mask;
+  }
+}
+
+static void handle_midi_message(const MidiMsg *msg) {
+  uint8_t status = msg->status & 0xF0;
+  uint8_t note = msg->d1;
+  uint8_t velocity = msg->d2;
+
+  if ((status == 0x90 || status == 0x80) && note >= 24 && note <= 31) {
+    uint8_t gate_index = note - 24; // 0-7
+    uint8_t gate_state = (status == 0x90 && velocity > 0) ? 1 : 0;
+    gate_set(gate_index, gate_state);
+  }
+}
+
 int main(void) {
+  GATE_DDR_B |= (1 << GATE_PIN_0); // Set PB0 as output
+  GATE_DDR_D |= 0xFE;              // Set PD1 to PD7 as outputs
+
+  // Gate wipe on boot - turn all gates on then off to verify they work
+  for (uint8_t i = 0; i < NUM_GATES; i++) {
+    gate_set(i, 1);
+  }
+  _delay_ms(100);
+  for (uint8_t i = 0; i < NUM_GATES; i++) {
+    gate_set(i, 0);
+  }
+
   USART_Init(MY_UBRR);
 
   MidiParser parser;
@@ -59,9 +103,7 @@ int main(void) {
     uint8_t byte;
     if (rb_pop(&byte)) {
       MidiMsg msg;
-      if (midi_parse(&parser, byte, &msg)) {
-        // TODO: Handle complete MIDI message
-      }
+      if (midi_parse(&parser, byte, &msg)) { handle_midi_message(&msg); }
     }
   }
   return 0;
