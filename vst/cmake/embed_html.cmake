69e5ca54ec39007fac28a8eea147bd4449183ddf
thorinf <thorin.j.a.farnsworth@gmail.com>
2026-04-20 23:43:01 +0100

    fix(vst): harden HTML embed build step

--- vst/cmake/embed_html.cmake
+++ vst/cmake/embed_html.cmake
@@ -1,4 +1,7 @@
+get_filename_component(output_dir "${OUTPUT}" DIRECTORY)
+file(MAKE_DIRECTORY "${output_dir}")
+
 file(READ "${INPUT}" html_content)
 file(WRITE "${OUTPUT}"
-  "#pragma once\n\nstatic const char kUIHTML[] = R\"html(${html_content})html\";\n"
+  "#pragma once\n\nstatic const char kUIHTML[] = R\"tram8_html(${html_content})tram8_html\";\n"
 )
